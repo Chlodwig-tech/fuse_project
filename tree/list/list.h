078480068c236f939f08f9822431dc392f5ead42
Szczepan Chojnowski <szczepan.chojnowski@tutanota.com>
2021-11-08 23:17:19 +0100

    list_functions

--- tree/list/list.h
+++ tree/list/list.h
@@ -9,4 +9,50 @@ typedef struct LIST{
     unsigned int size;
 }list;
 
+void list_init(list *l){
+    l->first=NULL;
+    l->last=NULL;
+    l->size=0;
+}
+
+void list_append(list *l,void *data){
+    if(l->first==NULL){
+        l->first=l->last=(list_element*)malloc(sizeof(list_element));
+        l->first->data=data;
+    }else{
+        list_element *helper=(list_element*)malloc(sizeof(list_element));
+        helper->data=data;
+        helper->next=NULL;
+        helper->previous=l->last;
+        l->last->next=helper;
+        l->last=helper;
+    }
+    l->size++;
+}
+
+void list_pop(list *l){
+    if(l->size>0){
+        list_element *helper=l->last;
+        if(l->first==l->last){
+            l->first=l->last=NULL;
+        }else{
+            l->last=l->last->previous;
+            l->last->next=NULL;
+        }
+        free(helper);
+        l->size--;
+    }
+}
+
+void *list_get_data(list *l,int index){
+    if(index<0 || index>=l->size){
+        return NULL;
+    }
+    list_element *helper=l->first;
+    for(int i=0;i<index;i++){
+        helper=helper->next;
+    }
+    return helper->data;
+}
+
 #endif // LIST_H
